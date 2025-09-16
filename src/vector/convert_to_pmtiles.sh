2ace9d08e84c2288dc659995b226857324e8dc73
EC2 Default User <ec2-user@ip-172-31-9-135.ap-southeast-2.compute.internal>
2025-09-16 22:13:08 +0000

    misc updates to pmtiles processing

--- src/vector/convert_to_pmtiles.sh
+++ src/vector/convert_to_pmtiles.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -euo pipefail
 
-S3_PREFIX="/vsis3/gpw-coastal-pollution-model-data-public-0001/inputs/land/global/global_watershed_bas/global_watershed_bas^0/"
+S3_PREFIX="/vsis3/gpw-coastal-pollution-model-data-public-0001/app/"
 
 if [ "$#" -ne 2 ]; then
     echo "Usage: $0 <output.pmtiles> <input.parquet>"
@@ -18,4 +18,4 @@ echo "Running ogr2ogr from:"
 echo "  Input:  ${INPUT_S3_PATH}"
 echo "  Output: ${OUTPUT_S3_PATH}"
 
-ogr2ogr -dsco MINZOOM=0 -dsco MAXZOOM=10 -f "PMTiles" "${OUTPUT_S3_PATH}" "${INPUT_S3_PATH}" -progress
+ogr2ogr -dsco MINZOOM=0 -dsco MAXZOOM=10 -f "PMTiles" "${OUTPUT_S3_PATH}" "${INPUT_S3_PATH}" -t_srs EPSG:3857 -progress
